f73c569ad7605a9136bc699f95afd7547bb9ec2d
Makoto Satake <satake.syukatsu@gmail.com>
2023-11-19 13:36:40 +0900

    ヘルプメッセージに日本語を追加した

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ use clap::Parser;
 
 #[derive(Parser)]
 #[clap(
-    name = "thue",
-    author = "satake",
-    version = "0.1.0",
-    about = "Translate english day of week to japanese one"
+    name = env!("CARGO_PKG_NAME"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    version = env!("CARGO_PKG_VERSION"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
 )]
 struct AppArg {
+    #[arg(help = "曜日の英語表記 / Day of the week in English")]
     en: Option<String>,
 }
 
@@ -30,12 +31,13 @@ fn main() {
 
     let arg: AppArg = AppArg::parse();
     if arg.en.is_some() {
-        let enceded = arg.en.unwrap().to_lowercase();
+        let input = arg.en.unwrap();
+        let enceded = input.to_lowercase();
         println!(
             "{}",
             en_ja_map
                 .get(&enceded)
-                .unwrap_or(&String::from("not day of week"))
+                .unwrap_or(&(input + &String::from(" is not day of week")))
         );
     } else {
         en_ja_list.iter().for_each(|(en, ja)| {
